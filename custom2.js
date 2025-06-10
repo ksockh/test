d3d86769f2c2296440ebc9df09c82f6d7951d13c
ksockh <ksockh789@gmail.com>
2025-06-10 16:12:45 +0900

    aaa

--- custom2.js
+++ custom2.js
@@ -6,7 +6,7 @@ $(window).on("load", function () {
       scrollTrigger: {
         trigger: section,
         start: "top top",
-        end: "bottom+100 top",
+        end: "bottom top",
         pin: true,
         pinSpacing: i === 3 ? true : false, // 마지막 섹션만 spacing 줌
         markers: true,
