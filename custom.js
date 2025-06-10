2e66b895d8c907bf7acf01f3f920b52fcd668fc8
ksockh <ksockh789@gmail.com>
2025-06-10 15:04:22 +0900

    12

--- custom.js
+++ custom.js
@@ -1,10 +1,13 @@
 $(function () {
   gsap.utils.toArray("#container section").forEach((section, i) => {
     console.log(i);
+    const content = section.querySelector(".txtBox");
     gsap.timeline({
       scrollTrigger: {
         trigger: section,
         start: "top top",
+        // end: () => "+=" + content.scrollHeight,
+        end: "+=3000",
         pin: true,
         // pinSpacing: false,
         pinSpacing: i === 3 ? true : false,
---
+++ custom2.js
@@ -0,0 +1,20 @@
+$(window).on("load", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray("#container section").forEach((section, i) => {
+    const sectionHeight = section.offsetHeight;
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: "top top",
+        end: () => "+=" + (section.scrollHeight + 1000),
+        pin: true,
+        pinSpacing: i === 3 ? true : false, // 마지막 섹션만 spacing 줌
+        markers: false,
+      },
+    });
+  });
+
+  ScrollTrigger.refresh();
+});
