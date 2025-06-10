1dcf29c05281dc0535d9b7f07e6a9e2259e8390a
ksockh <ksockh789@gmail.com>
2025-06-10 15:45:11 +0900

    aaa

--- custom2.js
+++ custom2.js
@@ -2,13 +2,14 @@ $(window).on("load", function () {
   gsap.registerPlugin(ScrollTrigger);
 
   gsap.utils.toArray("#container section").forEach((section, i) => {
-    const sectionHeight = section.offsetHeight;
+    const content = section.querySelector(".cloneWrap"); // ✅ 내부 콘텐츠 기준
+    const contentHeight = content.scrollHeight; // ✅ 실제 내용 전체 높이
 
     gsap.timeline({
       scrollTrigger: {
         trigger: section,
         start: "top top",
-        end: () => "+=" + (sectionHeight + 500),
+        end: () => "+=" + (contentHeight + 500),
         pin: true,
         pinSpacing: i === 3 ? true : false, // 마지막 섹션만 spacing 줌
         markers: true,
